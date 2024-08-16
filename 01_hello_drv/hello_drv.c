fef5fd2044b7b83cb9b8621e3434e19ba0df51c6
mowang <chenrz0411@qq.com>
2024-08-16 10:39:18 +0800

    错误修改

--- 01_hello_drv/hello_drv.c
+++ 01_hello_drv/hello_drv.c
@@ -59,7 +59,7 @@ static int __init hello_init(void)
 	printk("%s %s line %d\n", __FILE__, __FUNCTION__,__LINE);
 	hello_class = class_create(THIS_MODULE, "hello");
 	err = PTR_ERR(hello_class);
-	major = register_chrdev(MISC_MAJOR, "hello", &misc_fops);
+	major = register_chrdev(MISC_MAJOR, "hello", &hello_fops);
 	
 	device_create(hello_class, NULL, MKDEV(major, 0), NULL, "hello");
 	return 0;
